d770ac4da70b53fdd6b481d9cf11037b17332836
Toni Villena <tonivj5@gmail.com>
2015-12-26 01:41:36 +0100

    Actualizada información para pip

--- setup.py
+++ setup.py
@@ -5,19 +5,30 @@ setup(
     name='pyPassGenerator',
     version='1.0',
     description='Generate your random password',
-    long_description="It's a simple program that it's able of generate randoms, variables and with custom characters password.",
+    long_description="""It’s a simple program that it’s able of generate randoms, variables and with custom characters password.
+
+                        Documented in https://github.com/xxxtonixxx/pyPasswordGenerator/blob/master/README.md""",
     url='https://github.com/xxxtonixxx/pyPasswordGenerator.git',
     author='Toni Villena',
     author_email='tonivj5@gmail.com',
 
-    license='GNU GPL v2',
+    license='GNU GPLv2',
 
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
     ],
 
     keywords='password random variable pass generator',
